db9e21dbc4d9af99e0d7b83d9dc031db99077007
ThanhTung-2010 <dtong201003@gmail.com>
2024-09-27 23:54:02 +0700

    update

---
+++ Assets/Prefabs/bàn/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float lookSpeed = 2.0f;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    void Update()
+    {
+        // Mouse look
+        rotationX += Input.GetAxis("Mouse X") * lookSpeed;
+        rotationY -= Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0);
+
+        // Keyboard movement
+        float moveForward = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveSide = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(moveSide, 0, moveForward);
+    }
+}
